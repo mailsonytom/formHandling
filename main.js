6f717dfa615251939b25b4fd5d57c6817ca569cc
sony <mailsonytom@gmail.com>
2018-01-02 22:36:36 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+function buttonclick()
+		{
+			var x=document.forms["userform"]["userid"].value;
+			var y=document.getElementById("display");
+        	y.innerHTML= "The inputted Username is " + x
+        }
